7c350db80b3b0a06833ef3a61deab470ad2df40c
iainjmitchell <iainjmitchell@gmail.com>
2013-09-23 20:43:04 +0100

    Starting to add full lifecycle test

---
+++ test/test-repository-lifecycle.js
@@ -0,0 +1,65 @@
+var should = require('chai').should(),
+	MongoRepository = require('../lib/repository').Repository;
+
+describe('When we want to keep track of cats', function(){
+	var connectionString = 'mongodb://mongorepository:manky@paulo.mongohq.com:10051/mongorepositorytest',
+		mongoRepository = new MongoRepository(connectionString, 'cat');
+
+	describe('When I have no cats', function(){
+		describe('And I try to find all cats', function(){
+			it('Then no cats are returned :(', function(done){
+				mongoRepository.find({}, function(cats){
+					cats.length.should.equal(0);
+					done();
+				});
+			});
+		});
+	});
+
+	describe('When I add tom cat', function(){
+		var tomCat = {name: 'TomCat'};
+		it('Then the callback is triggered', function(done){
+			mongoRepository.add(tomCat, done);
+		});
+
+		describe('And I try to find tom cat', function(){
+			it('Then one cat is returned :)', function(done){
+				mongoRepository.find(tomCat, function(cats){
+					cats.length.should.equal(1);
+					done();
+				});
+			});
+
+			describe('And I update tom cat to include age', function(){
+				describe('And I try to find tom cat', function(){
+					it('Then cat returned includes age :{', function(done){
+						var age = 10;
+						mongoRepository.update(tomCat, {name: 'TomCat', age: age});
+						mongoRepository.find(tomCat, function(cats){
+							cats[0].age.should.equal(age);
+							done();
+						});
+					});
+				});
+			});
+
+			describe('And I remove tom cat', function(){
+				describe('And I try to find tom cat', function(){
+					it('Then no cats are returned :(', function(done){
+						mongoRepository.remove(tomCat);
+						mongoRepository.find(tomCat, function(cats){
+							cats.length.should.equal(0);
+							done();
+						});
+					});
+				});
+			});
+		});
+
+		describe('When I add a whole host of kitties', function(){
+			describe('And I try to find all the cats with age of 10', function(){
+
+			});
+		});
+	});
+});
